ef2c78fd31ca3b2ca158d8e11b73a2a70f8abc6e
XandO <frankie.the.coin@gmail.com>
2010-10-08 11:25:48 +0200

    Position counts. Names in urls.py are matched until first correct. First correct in that case was list.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('blog.views',
+
+                       url(r'^list/$', 'list'),
                        url(r'^(?P<name>.+)/new/$','new'),
                        url(r'^(?P<name>.+)', 'blog'),
-                       url(r'^list/$', 'list'),
-                       
+
                        )
