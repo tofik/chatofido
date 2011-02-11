47be82bf9e3aab3a4e887c1651006c9cbd8f0e50
Tomek Filipczuk <tomek.filipczuk@gmail.com>
2011-02-11 21:33:12 +0100

    Updates UI for adding new post, image_post and comment

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     blog = models.ForeignKey(Blog)
     content = models.TextField()
     created = models.DateTimeField(auto_now_add = True)
-    title = models.CharField(max_length=60)
+    title = models.CharField(max_length=40)
     author = models.CharField(max_length=20)
 
 class Image(Post):
