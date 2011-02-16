c59957c35ca1bf976cf48a3d306c594094a8184e
Tomasz Filipczuk <tomasz.filipczuk@softica.org>
2011-02-16 16:14:06 +0100

    Modifies admin site - minor updates for Comment and Post views.

--- blog/admin.py
+++ blog/admin.py
@@ -6,6 +6,10 @@ class BlogAdmin(admin.ModelAdmin):
     list_filter = ['modified']
     search_fields = ['name']
 
+class CommentsInLine(admin.TabularInline):
+    model = Comment
+    extra = 3
+
 class PostAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Basic information', {'fields': ['author', 'title']}),
@@ -14,8 +18,7 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'created', 'blog')
     list_filter = ['created', 'blog']
     search_fields = ['title', 'author']
-
-
+    inlines = [CommentsInLine]
 
 class ImageAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -28,13 +31,13 @@ class CommentAdmin(admin.ModelAdmin):
         ('Basic information', {'fields': ['author']}),
         ('Content'          , {'fields': ['content', 'post'], 'classes': ['collapse']})
         ]
-    list_filter = ('author', 'created', 'post')
 
     def get_blog(self):
         return Blog.objects.get(post=self.post)
     get_blog.short_description = "Blog"
 
-    list_display = ('author', 'created', 'content', get_blog)
+    list_filter = ('author', 'created', 'post')
+    list_display = ('author', 'created', 'content', 'post', get_blog)
 
     
      
